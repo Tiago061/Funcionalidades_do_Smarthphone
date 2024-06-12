4b2d915f44aaf65fc6c3131a6eddf8dcda48c818
Tiago061 <thenriqueleite277@gmail.com>
2024-06-12 11:20:10 -0300

    Métodos de Reproduzir música criado, e método de aparelho telefonico criados

--- src/Devices/Iphone.java
+++ src/Devices/Iphone.java
@@ -5,38 +5,93 @@ import Functions.NavegadorInternet;
 import Functions.ReprodutorMusical;
 
 public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    private String telefone;
+    private String musicaAtual;
+    private String site;
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setMusicaAtual(String musicaAtual) {
+        this.musicaAtual = musicaAtual;
+    }
+
+    public String getMusicaAtual() {
+        return musicaAtual;
+    }
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
     @Override
     public void tocarMusica() {
+        if(musicaAtual == null){
+            System.out.println("Nenhuma música selecionada, por favor, selecione uma música.");
+        }else{
+            System.out.println("Tocando música: " + musicaAtual);
+        }
 
     }
 
     @Override
     public void pausarMusica() {
+        System.out.println("Música Pausada: " + musicaAtual);
 
     }
 
     @Override
     public void selecionarMusica(String musica) {
+        if (musica == null){
+            System.out.println("Selecione uma música ");
+        }else {
+            musicaAtual = musica;
+            System.out.println("Música Selecionada " + musicaAtual);
+        }
 
     }
 
     @Override
     public void ligar(String numero) {
-
+        if(numero != telefone){
+            System.out.println("Esse número não existe");
+        }else {
+            telefone = numero;
+            System.out.println("Ligando para: " + telefone);
+        }
     }
+
 
     @Override
     public void atender() {
-
+        boolean ligacaoAtendida = false;
+        if (ligacaoAtendida == true){
+            System.out.println("Chamada atendida");
+        }else{
+            System.out.println("ligação perdida");
+        }
     }
+
 
     @Override
     public void iniciarCorreioVoz() {
-
+        boolean ligarParaPessoa = false;
+        if(!ligarParaPessoa == true){
+            System.out.println("Deixar recado de voz");
+        }
     }
 
     @Override
     public void exibirPagina(String url) {
+
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import Devices.Iphone;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Iphone iphone = new Iphone();
+        System.out.println("Selecione a música: ");
+        String musica = sc.next();
+        iphone.setMusicaAtual(musica);
+        iphone.selecionarMusica(iphone.getMusicaAtual());
+        iphone.tocarMusica();
+        iphone.pausarMusica();
 
+
+        System.out.println("Digite o número de telefone: ");
+        String telefone = sc.next();
+        iphone.setTelefone(telefone);
+        iphone.ligar(iphone.getTelefone());
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+
+
+        sc.close();
         }
     }
